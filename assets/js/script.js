ce142a6de3c4d1c447a20a20484fe63f8547f259
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2021-02-06 01:59:44 +0100

    compare

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+window.addEventListener("scroll", ()=>{
+const formContainer = document.querySelector("#formContainer");
+formContainer.classList.toggle('fixed', window.scrollY > 350);
+});
+
+
+
+function myFunc(){
+        alert("Thank you! Your Booking Has been Made!");
+    }
+
+    
+
+
+ 
